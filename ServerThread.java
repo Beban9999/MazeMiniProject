5069e022639b2a7dc5d9d2833fa4da0afd342023
Aleksa Aleksic <54003004+Beban9999@users.noreply.github.com>
2022-05-19 23:11:23 +0200

    Update ServerThread.java

--- ServerThread.java
+++ ServerThread.java
@@ -23,6 +23,12 @@ public class ServerThread extends Thread{
         start();
 
     }
+    public boolean isValid(int x, int y, String[][] grid, boolean[][] visited){
+        if ((x >= 0 && y >=0) && (x < 9 && y < 9) && (!grid[x][y].equals("#") && (!visited[x][y]))){
+            return true;
+        }
+        return false;
+    }
     public void run() {
 
         try {
@@ -55,11 +61,11 @@ public class ServerThread extends Thread{
                     for(int j = 0; j < 9; j++){
                         if(table[i][j].equals("R")){
                             Rcord[0] = i; Rcord[1] = j;
-                            System.out.println(Rcord[0]+" "+ Rcord[1]);
+                            //System.out.println(Rcord[0]+" "+ Rcord[1]);
                         }
                         if(table[i][j].equals("E")){
                             Ecord[0] = i; Ecord[1] = j;
-                            System.out.println(Ecord[0]+" "+ Ecord[1]);
+                            //System.out.println(Ecord[0]+" "+ Ecord[1]);
                             Eovi.add(i);
                             Eovi.add(j);
                         }
@@ -68,60 +74,52 @@ public class ServerThread extends Thread{
                     Server.tas.append("\n");
                 }
 
-                for(int i = 0; i < Eovi.size(); i++){
-
-                    System.out.println(Eovi.get(i)+ " "+ Eovi.get(i+1));
-                    i++;
-                }
-
-                br = 0;
-
-                while(br != Eovi.size()){
-                    Ecord[0] = Eovi.get(br); Ecord[1] = Eovi.get(++br);
-
-                    while((Rcord[0] != Ecord[0]) || (Rcord[1] != Ecord[1])){
-                        if((Rcord[1] < Ecord[1]) && (!table[Rcord[0]][Rcord[1]+1].equals("#"))){
-                            Rcord[1]++;
-                            odg += "R";
-                            System.out.println(odg);
-                            if((Ecord[0] == Rcord[0]-1) && !(table[Rcord[0]-1][Rcord[1]].equals("#"))) {
-                                Rcord[0]--;
-                                odg += "U";
-                                System.out.println(odg);
-                            }
-                        }
-                        else{
-                            if(table[Rcord[0]+1][Rcord[1]].equals("#")){
-                                Rcord[1]++;
-                                odg += "R";
-                                System.out.println(odg);
-                            }
-                            if(Rcord[0] != Ecord[0]){
 
 
-
-                                Rcord[0]++;
-                                odg+= "D";
-                                System.out.println(odg);
-
-                                if((Ecord[1] == Rcord[1]-1) && !(table[Rcord[0]][Rcord[1]-1].equals("#"))){
-                                    Rcord[1]--;
-                                    odg += "L";
-                                    System.out.println(odg);
-                                }
-
-
-
-                            }
+                while(Eovi.size() > 0){
+                    boolean [][] visited = new boolean[9][9];
+                    for(int i = 0; i < 9; i++){
+                        for(int j = 0; j < 9; j++){
+                            visited[i][j] = false;
                         }
                     }
+                    Ecord[0] = Eovi.get(0);
+                    Eovi.remove(0);
+                    Ecord[1] = Eovi.get(0);
+                    Eovi.remove(0);
+                    System.out.println(Ecord[0] + " "+ Ecord[1]);
+                    ArrayList<Cell> q = new ArrayList<>();
+                    q.add(new Cell(Rcord[0], Rcord[1], ""));
+                    visited[Rcord[0]][Rcord[1]] = true;
+                    while(q.size() > 0){
+                        Cell source = q.get(0);
+                        q.remove(source);
+                        //System.out.println(source.row +" "+source.col +" "+ source.path);
+                        if (source.row == Ecord[0] && source.col == Ecord[1]){
+                            odg += source.path+" ";
+                            break;
+                        }
 
-                    odg += "  ";
-                    Rcord[0] = 1; Rcord[1] = 1; br++;
+                        if (isValid(source.row-1, source.col, table, visited)){
+                            q.add(new Cell(source.row-1, source.col, source.path+"U"));
+                            visited[source.row-1][source.col] = true;
+                        }
+                        if (isValid(source.row + 1, source.col, table, visited)){
+                            q.add(new Cell(source.row+1, source.col, source.path+"D"));
+                            visited[source.row+1][source.col] = true;
+                        }
+                        if (isValid(source.row, source.col-1, table, visited)){
+                            q.add(new Cell(source.row, source.col-1, source.path+"L"));
+                            visited[source.row][source.col-1] = true;
+                        }
+                        if (isValid(source.row, source.col+1, table, visited)){
+                            q.add(new Cell(source.row, source.col+1, source.path+"R"));
+                            visited[source.row][source.col+1] = true;
+                        }
+
+
+                    }
                 }
-
-
-
 
 
             }
